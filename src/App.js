7f14d43f7c77abb1c430bb114a3795e108d1d6af
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-02-02 12:18:47 -0600

    Added prettier file w/ default settings. Saved all files to update w/ new formatting settings.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h2", {}, props.name),
-    React.createElement("p", {}, props.description)
+    React.createElement("p", {}, props.description),
   ]);
 };
 
 const App = () => {
-  return React.createElement("div", {},
-    [
-      React.createElement("h1", {}, "Padre Gino's"),
-      React.createElement(Pizza, {
-        name: "The Pepperoni Pizza",
-        description: "Red sauce, mozz, pepperoni"
-      }),
-      React.createElement(Pizza, {
-        name: "The Cheesiest Pizza",
-        description: "Red sauce, mozz, some other cheese"
-      }),
-      React.createElement(Pizza, {
-        name: "The Hawaiaan Shirt",
-        description: "Red sauce, mozz, ham, pineapple, olives"
-      }),
-      React.createElement(Pizza, {
-        name: "The Green Machine",
-        description: "Red sauce, mozz, spinach, broccoli, cherry tomatoes"
-      }),
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Red sauce, mozz, pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Cheesiest Pizza",
+      description: "Red sauce, mozz, some other cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiaan Shirt",
+      description: "Red sauce, mozz, ham, pineapple, olives",
+    }),
+    React.createElement(Pizza, {
+      name: "The Green Machine",
+      description: "Red sauce, mozz, spinach, broccoli, cherry tomatoes",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
