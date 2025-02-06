0e31c40d8eed3c46e88eff890369f6f8b73d312f
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-02-05 22:24:30 -0600

    Using useState hook to update values in individual form selections.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Hawaiian"
-        description="Marinara, mozz, ham, and pineapple"
-        image="/public/pizzas/hawaiian.webp"
-      />
-      <Pizza
-        name="Pepperoni"
-        description="Marinara, mozz, pepperoni"
-        image="/public/pizzas/pepperoni.webp"
-      />
-      <Pizza
-        name="Big Meat"
-        description="Marinara, mozz, and all da meats"
-        image="/public/pizzas/big_meat.webp"
-      />
+      <Order />
     </div>
   );
 };
