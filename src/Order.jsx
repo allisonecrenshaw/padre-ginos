d3ffa72fe41ffccb563ef9b1a7346f0e1bbc4bf9
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-02-06 00:04:57 -0600

    Removed await.

--- src/Order.jsx
+++ src/Order.jsx
@@ -21,8 +21,6 @@ export default function Order() {
   }
 
   async function fetchPizzaTypes() {
-    await new Promise((resolve) => setTimeout(resolve, 3000)); // remove this later, just to show you the loading state
-
     const pizzasRes = await fetch("/api/pizzas");
     const pizzasJson = await pizzasRes.json();
     setPizzaTypes(pizzasJson);
